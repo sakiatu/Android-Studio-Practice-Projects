1f32cb5d53998fe36ba8a17b3034f0a09c7e40e2
sakib20 <sakib20@iut-dhaka.edu>
2020-03-26 11:15:45 +0600

    BroadCast Receiver

--- BroadCastReciever/app/src/main/java/apps/simplee/broadcastreciever/MainActivity.java
+++ BroadCastReciever/app/src/main/java/apps/simplee/broadcastreciever/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            boolean value = intent.getBooleanExtra("state",false);
-            if(value)
+            boolean value = intent.getBooleanExtra("state", false);
+            if (value)
                 setIcon(R.drawable.airplane_on);
             else
                 setIcon(R.drawable.airplane_off);
@@ -33,10 +33,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFlightModeReceiver();
+        findViewById(R.id.customButton).setOnClickListener(this::clicked);
 
+    }
+
+    private void clicked(View view) {
+        if(view.getId()==R.id.customButton){
+            sendBroadcast(new Intent("apps.simplee.action.CUSTOM_BROADCAST"));
+        }
+    }
+
+    private void initFlightModeReceiver() {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
-
-        registerReceiver(receiver,filter);
+        registerReceiver(receiver, filter);
     }
 }
--- BroadCastReciever/app/src/main/java/apps/simplee/broadcastreciever/MyReceiver.java
+++ BroadCastReciever/app/src/main/java/apps/simplee/broadcastreciever/MyReceiver.java
@@ -3,13 +3,21 @@ package apps.simplee.broadcastreciever;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class MyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        Log.i("ABCD",""+ intent.getAction());
     }
 }
+
+//these lines have been added in manifest
+/*
+<intent-filter>
+<action android:name="android.intent.action.AIRPLANE_MODE"/>
+<action android:name="apps.simplee.action.CUSTOM_BROADCAST" />
+
+</intent-filter>*/
